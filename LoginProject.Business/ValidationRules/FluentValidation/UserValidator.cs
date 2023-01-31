ee5b299f7cd2b9ecaec0374b1151f834499dbbdb
Hakan Özdemir <hakan97ozdemir@gmail.com>
2023-01-31 03:19:36 +0300

    Validator Update

--- LoginProject.Business/ValidationRules/FluentValidation/UserValidator.cs
+++ LoginProject.Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -5,11 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LoginProject.Business.ValidationRules.FluentValidation
 {
-    public class UserValidator:AbstractValidator<User>
+    public class UserValidator : AbstractValidator<User>
     {
         public UserValidator()
         {
@@ -17,8 +18,18 @@ namespace LoginProject.Business.ValidationRules.FluentValidation
             RuleFor(p => p.Username).MinimumLength(5).WithMessage("Kullanıcı Adı en az 5 karakterden oluşmalıdır.");
             RuleFor(p => p.Username).EmailAddress().WithMessage("Kullanıcı Adınız uygun formatta değil.");
             RuleFor(p => p.Password).NotEmpty().WithMessage("Şifre boş bırakılamaz.");
-            RuleFor(p => p.Password).MinimumLength(5).WithMessage("Şifre en az 5 karakterden oluşmalıdır.");
-            //RuleFor(p => p.Password).Must(p => p > 99 && p < 10000000).WithMessage("Şifreniz sadece sayılardan oluşabilir.");
+            RuleFor(p => p.Password).Matches("[0-9]+").WithMessage("Parolanız sayısal karakterlerden oluşmalıdır.");
+
+
+        }
+        private bool HasValidPassword(string pw)
+        {
+            var lowercase = new Regex("[a-z]+");
+            var uppercase = new Regex("[A-Z]+");
+            var digit = new Regex("(\\d)+");
+            var symbol = new Regex("(\\W)+");
+
+            return (!lowercase.IsMatch(pw) && !uppercase.IsMatch(pw) && digit.IsMatch(pw) && !symbol.IsMatch(pw) );
         }
     }
 }
